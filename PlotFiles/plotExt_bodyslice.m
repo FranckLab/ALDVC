23c21a6d4b5f85851e1cd0736d622e3501082c8c
JIN YANG <30322736+jyang526843@users.noreply.github.com>
2020-07-26 13:40:22 -0500

    Add files via upload

---
+++ PlotFiles/plotExt_bodyslice.m
@@ -0,0 +1,120 @@
+%%%%%%%%%%%%% Extensions for body and slice plottings %%%%%%%%%%%%%%%%
+% Extension of ALDVC code: to plot solved displacements and strains
+%
+%   INPUT: main_ALDVC.m saved matfile after Section 8
+%          Feel free to modify this code based on your purpose. 
+%          I comment "#TOMODIFY" where you can easily modify the code.
+%
+%   OUTPUT: body plots and slice plots for solved displacements and strains
+%
+% -------------------------------------------
+% Author: Jin Yang, Postdoc UW-Madison; PhD '19 Caltech;
+% Contact: jyang526@wisc.edu;  aldicdvc@gmail.com
+% Date: 2020.07
+% --------------------------------------------------- 
+%%
+addpath('./func','./PlotFiles');
+% You can directly start these plottings from saved results matfile
+% #TOMODIFY:
+ImgSeqNum = 2; % Define frame sequence number, starting from 2, the first ref image always has "ImgSeqNum=1"
+
+%% Body view of solved displacements
+Plotdisp03(ResultDisp{ImgSeqNum-1}.U,DVCmesh.coordinatesFEM,DVCmesh.elementsFEM,DVCpara.PlotComponentEachOrAll);
+% #TOMODIFY:
+% Change "DVCpara.PlotComponentEachOrAll" to "Individual" to plot each displacement component in a single figure 
+% Plotdisp03(ResultDisp{ImgSeqNum-1}.U,DVCmesh.coordinatesFEM,DVCmesh.elementsFEM,'Individual');
+
+%% Slice view of solved displacements
+% To view slice of solved displacements
+xyz0 = DVCmesh.xyz0; MNL = size(xyz0.x); % xyz0 is the xyz coordinates of nodal points
+U = ResultDisp{ImgSeqNum-1}.U; % U is a long vector for measured U = [Ux_pt1, Uy_pt1, Uz_pt1,  Ux_pt2, Uy_pt2, Uz_pt2, ... ]';
+Ux = reshape(U(1:3:end),MNL); Uy = reshape(U(2:3:end),MNL); Uz = reshape(U(3:3:end),MNL);
+% Ux,Uy,Uz are x-, y-, and z- components of solved displacements, whose sizes are same with xyz0.x or xyz0.y or xyz0.z
+
+figure, imagesc3D(Ux); % There is no units in 'imagesc3D' function 
+% Coordinates in this plot: going downwards is x+ direction, going right is y+ direction
+
+
+% #TOMODIFY:
+sliceNum = 6; % z-slice #
+
+figure, surf(squeeze(xyz0.x(:,:,sliceNum)),squeeze(xyz0.y(:,:,sliceNum)),squeeze(Ux(:,:,sliceNum))); 
+view(2); axis equal; axis tight; set(gca,'fontsize',18); cb = colorbar;
+title(['Solved x-disp at z-slice #',num2str(sliceNum),'  (unit:vx)'],'fontweight','normal');
+
+
+
+%% Body view of solved strains
+xyz0 = DVCmesh.xyz0; % xyz0 is the xyz coordinates of nodal points
+F = ResultDefGrad{ImgSeqNum-1}.F; % F is a long vector for solved F = (grad_X x - I) =  ...
+% [F11_pt1, F21_pt1, F31_pt1, F12_pt1, F22_pt1, F32_pt1, F13_pt1, F23_pt1, F33_pt1, ...
+% [F11_pt2, F21_pt2, F31_pt2, F12_pt2, F22_pt2, F32_pt2, F13_pt2, F23_pt2, F33_pt2, ... ]';
+Plotstrain03(full(F),xyz0.x, xyz0.y, xyz0.z,0,DVCpara.PlotComponentEachOrAll);
+% #TOMODIFY:
+% Change "DVCpara.PlotComponentEachOrAll" to "Individual" to plot each strain component in a single figure 
+% Plotstrain03(full(F),xyz0.x, xyz0.y, xyz0.z,0,'Individual');
+
+% ===================================
+% If you want to plot strains after executing Section 8 (where additional modifications can be done)
+xyz0 = DVCmesh.xyz0; MNL = size(xyz0.x);
+FStraintemp = ResultStrain{ImgSeqNum-1}.Strain; coordinatesFEMStrain = ResultStrain{ImgSeqNum-1}.coordinatesFEMStrain;
+tempRad=0; 
+while abs(prod(MNL-tempRad)-size(coordinatesFEMStrain,1))>0 && tempRad<size(coordinatesFEMStrain,1)
+    tempRad = tempRad+2;
+end
+Rad=repmat(tempRad/2,1,3); % Or manually assign the value of "Rad" used in previous Section 8
+ 
+% each displacement component in a single figure
+Plotstrain03(full(FStraintemp),xyz0.x(1+Rad(1):MNL(1)-Rad(1),1+Rad(2):MNL(2)-Rad(2),1+Rad(3):MNL(3)-Rad(3)), ...
+    xyz0.y(1+Rad(1):MNL(1)-Rad(1),1+Rad(2):MNL(2)-Rad(2),1+Rad(3):MNL(3)-Rad(3)), ...
+    xyz0.z(1+Rad(1):MNL(1)-Rad(1),1+Rad(2):MNL(2)-Rad(2),1+Rad(3):MNL(3)-Rad(3)),0,DVCpara.PlotComponentEachOrAll);
+% #TOMODIFY:
+% Change "DVCpara.PlotComponentEachOrAll" to "Individual" to plot each strain component in a single figure
+% Plotstrain03(full(FStraintemp),xyz0.x(1+Rad(1):MNL(1)-Rad(1),1+Rad(2):MNL(2)-Rad(2),1+Rad(3):MNL(3)-Rad(3)), ...
+%     xyz0.y(1+Rad(1):MNL(1)-Rad(1),1+Rad(2):MNL(2)-Rad(2),1+Rad(3):MNL(3)-Rad(3)), ...
+%     xyz0.z(1+Rad(1):MNL(1)-Rad(1),1+Rad(2):MNL(2)-Rad(2),1+Rad(3):MNL(3)-Rad(3)),0,'Individual');
+  
+
+%% Slice view of solved strains
+xyz0 = DVCmesh.xyz0; MNL = size(xyz0.x); % xyz0 is the xyz coordinates of nodal points
+F = ResultDefGrad{ImgSeqNum-1}.F; % F is a long vector for solved F = (grad_X x - I) =  ...
+% [F11_pt1, F21_pt1, F31_pt1, F12_pt1, F22_pt1, F32_pt1, F13_pt1, F23_pt1, F33_pt1, ...
+% [F11_pt2, F21_pt2, F31_pt2, F12_pt2, F22_pt2, F32_pt2, F13_pt2, F23_pt2, F33_pt2, ... ]';
+ 
+F11 = reshape(F(1:9:end),MNL); F21 = reshape(F(2:9:end),MNL); F31 = reshape(F(3:9:end),MNL);
+F12 = reshape(F(4:9:end),MNL); F22 = reshape(F(5:9:end),MNL); F32 = reshape(F(6:9:end),MNL);
+F13 = reshape(F(7:9:end),MNL); F23 = reshape(F(8:9:end),MNL); F33 = reshape(F(9:9:end),MNL);
+% Individual components of solved strains, whose sizes are same with xyz0.x or xyz0.y or xyz0.z
+
+figure, imagesc3D(F11); 
+
+% #TOMODIFY:
+sliceNum = 6; % z-slice #
+
+figure, surf(squeeze(xyz0.x(:,:,sliceNum)),squeeze(xyz0.y(:,:,sliceNum)),squeeze(F11(:,:,sliceNum))); 
+view(2); axis equal; axis tight; set(gca,'fontsize',18); cb = colorbar;
+title(['Strain exx at z-slice #',num2str(sliceNum),'  (unit:vx)'],'fontweight','normal');
+
+
+% ===================================
+% If you want to plot strains after executing Section 8 (where additional modifications can be done)
+FStraintemp = ResultStrain{ImgSeqNum-1}.Strain; coordinatesFEMStrain = ResultStrain{ImgSeqNum-1}.coordinatesFEMStrain;
+xList=unique(coordinatesFEMStrain(:,1)); yList=unique(coordinatesFEMStrain(:,2)); zList=unique(coordinatesFEMStrain(:,3));
+MNLtemp=[length(xList),length(yList),length(zList)]; [yGrid,xGrid]=meshgrid(yList,xList);
+ 
+F11 = reshape(FStraintemp(1:9:end),MNLtemp); F21 = reshape(FStraintemp(2:9:end),MNLtemp); F31 = reshape(FStraintemp(3:9:end),MNLtemp);
+F12 = reshape(FStraintemp(4:9:end),MNLtemp); F22 = reshape(FStraintemp(5:9:end),MNLtemp); F32 = reshape(FStraintemp(6:9:end),MNLtemp);
+F13 = reshape(FStraintemp(7:9:end),MNLtemp); F23 = reshape(FStraintemp(8:9:end),MNLtemp); F33 = reshape(FStraintemp(9:9:end),MNLtemp);
+
+figure, imagesc3D(F11); 
+
+% #TOMODIFY:
+sliceNum = 6; % z-slice #
+figure, surf(xGrid,yGrid,squeeze(F11(:,:,sliceNum))); 
+view(2); axis equal; axis tight; set(gca,'fontsize',18); cb = colorbar;
+title(['Strain exx at z-slice #',num2str(sliceNum),'  (unit:vx)'],'fontweight','normal');
+
+
+
+
+
